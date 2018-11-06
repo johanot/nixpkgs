ca5f0420a6229e3fbf63a9ed8b2119de81b1c496
Kristoffer <kristoffer@microdisko.no>
2018-11-06 18:32:06 +0100

    ceph: 13.2.2 -> 13.2.4, fixes
    ceph: erroneous comma, fix target dependencies
    
    ceph: fix lttng-ust alias
    
    ceph: Use system boost
    
    ceph: Add documentation on python27 usage
    
    ceph: substitute substituteInPlace for patch, init ceph-client
    
    ceph: include ceph-volume python tool in output
    
    ceph: fix rados python module path
    
    ceph: Fix callPackage to callPackages
    
    The function callPackage masks the override attribute and makes ceph
    non-overridable by the user. The function callPackages fixes this
    by adding the override attribute on each set.
    
    ceph: 13.2.2 -> 13.2.4
    
    ceph: remove argparse
    
    ceph: remove wkennington as maintainer

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2179,9 +2179,11 @@ in
   nrg2iso = callPackage ../tools/cd-dvd/nrg2iso { };
 
   libceph = ceph.lib;
-  ceph = callPackage ../tools/filesystems/ceph {
+  inherit (callPackages ../tools/filesystems/ceph {
     boost = boost167.override { enablePython = true; };
-  };
+  })
+    ceph
+    ceph-client;
   ceph-dev = ceph;
 
   inherit (callPackages ../tools/security/certmgr { })
